5e7cec209f68aeeb55efcc89054adc090c9dc47b
cecilia735 <Cecilia@Cecilias-MacBook-Pro.local>
2021-04-09 21:37:24 +0200

    Session5HW_BestPracticesInR_Cecilia

--- session4.R
+++ session4.R
@@ -1,36 +1,60 @@
+# Created on 9th of April 2021
+# This is code for behavioral anlyses for the project "Electrophysiological Signatures of Conceptual and Lexical Retrieval from Semantic Memory"
+# In this script we generate a quantile plot and compute t-tests
+# Code developed by Cecilia Husta
+
+# Data description: Data belongs to two experiments, verbal and nonverbal. 
+# Both experiments included trials that contrained either constraining or nonconstraining context that indicated to different digrees what the target picture was going to be.
+# The target pictures had to be named.
+# Expectations: RTs will be shorter folowing constraining context (in both expeirments)
+
+
+# packages
 library(tidyverse)
 library(purrr)
-library(lme4)
-library(nlme)
-library(lmerTest)
 
-#### ####
+# input files
+input_file_VerbalRTs <- "Verbal_NewRT.txt" 
+input_file_VerbalCloze <- "Cloze_ID_Verbal.txt" 
+input_file_VerbalID <- "_ID_Verbal.txt"           # 18 files (for each subject) with item IDs for verbal task (e.g. P_13_ID_Verbal.txt)
+input_file_NonverbalRTs <- "Nonverbal_NewRT.txt" 
+input_file_NonverbalCloze <- "Cloze_ID.txt" 
+input_file_NonverbalID <- "_ID.txt"                  # 18 files (for each subject) with item IDs for nonverbal task (e.g. P_13_ID.txt)
+
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
 # Load Verbal Data
+
+# Dataset includes 18 subjects (subj 1 & 9 are excluded because of EEG artifacts), 120 trials
+# constrained or unconstrained condition, RTs in ms, accuracy (0 = incorrect, 1 = correct,2 = synonym of a correct answer)
 verbal <-
-  read_tsv("Verbal_NewRT.txt", F) 
-colnames(verbal) = c("sbj", "trial", "condition", "rt", "accuracy")
-# subject number, trial number, condition, reaction time, and the answer 
-verbal <- verbal %>% 
-  mutate(task = "verbal", 
-         cond = if_else(condition == 11, "constrained", "unconstrained"))
+  read_tsv(input_file_VerbalRTs, F) 
+  colnames(verbal) = c("sbj", "trial", "condition", "rt", "accuracy")
+  verbal <- verbal %>% 
+  mutate(task = "verbal", cond = if_else(condition == 11, "constrained", "unconstrained"))
 verbal
 
+# Load cloze probability information based on trial number in the verbal dataset
+
+# Load file with cloze probabilities and item Ids
 ppNumbers <- c(2:8, 10:20)
 ID <- data.frame()
 Cloze_verbal <- data.frame()
-Cloze_ID <- read_tsv("Cloze_ID_Verbal.txt", F)
+Cloze_ID <- read_tsv(input_file_VerbalCloze, F)
 colnames(Cloze_ID) = c("ID", "Cloze")
 
-# Load all Item Ids
+# Load all item Ids per participant
 for ( i in ppNumbers) {
-  file <- gsub(" ", "", paste("P", i, "_ID_Verbal.txt"))
+  file <- gsub(" ", "", paste("P", i, input_file_VerbalID))
   ID_participant <- read_tsv(file, F)
   colnames(ID_participant) = c("trial_num", "ID")
   ID <- rbind(ID, ID_participant)
 }
 
-Verbal_cloze <- cbind(verbal,ID[,2]) # Merge the Item ID to the verbal data
+# Merge the item Ids to the verbal data
+Verbal_cloze <- cbind(verbal,ID[,2])
 
 # Add Cloze probabilities based on the item IDs
 trials <- c(1:2160)
@@ -38,9 +62,6 @@ for (i in trials) {
   x <- which(Cloze_ID[,1] == Verbal_cloze[i,8], arr.ind=TRUE)
   Verbal_cloze[i,9] <- Cloze_ID[x[,1],2]
 }
-
-
-print(Verbal_cloze[1,5])
 
 indexa <- match(Verbal_cloze[with(Verbal_cloze, sbj == 11 & trial == 118), ])
 
@@ -57,29 +78,39 @@ for (i in trials){
   }
 }
 
-# Load Nonverbal Data
-nverbal <-
-  read_tsv("Nonverbal_NewRT.txt", F)
-colnames(nverbal) = c("sbj", "trial", "condition", "rt", "accuracy")
-nverbal <- nverbal %>% 
-  mutate(task = "nonverbal", 
-         cond = if_else(condition == 3, "constrained", "unconstrained"))
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
-# Load all Item Ids
+# Load Nonverbal Data
+
+# Dataset includes the same 18 subjects as verbal experiment (subj 1 & 9 are excluded because of EEG artifacts), 120 trials
+# constrained or unconstrained condition, RTs in ms, accuracy (0 = incorrect, 1 = correct,2 = synonym of a correct answer)
+nverbal <-
+  read_tsv(input_file_NonverbalRTs, F)
+  colnames(nverbal) = c("sbj", "trial", "condition", "rt", "accuracy")
+  nverbal <- nverbal %>% 
+  mutate(task = "nonverbal", cond = if_else(condition == 3, "constrained", "unconstrained"))
+nverbal
+
+# Load cloze probability information based on trial number in the nonverbal dataset
+
+# Load file with cloze probabilities and item Ids
 ppNumbers <- c(2:8, 10:20)
 ID <- data.frame()
 Cloze_verbal <- data.frame()
-Cloze_ID <- read_tsv("Cloze_ID.txt", F)
+Cloze_ID <- read_tsv(input_file_NonverbalCloze, F)
 colnames(Cloze_ID) = c("ID", "Cloze")
 
+# Load all item Ids per participant
 for ( i in ppNumbers) {
-  file <- gsub(" ", "", paste("P", i, "_ID.txt"))
+  file <- gsub(" ", "", paste("P", i, input_file_NonverbalID))
   ID_participant <- read_tsv(file, F)
   colnames(ID_participant) = c("trial_num", "ID")
   ID <- rbind(ID, ID_participant)
 }
 
-Nonverbal_cloze <- cbind(nverbal,ID[,2]) # Merge the ID to the nonverbal data
+# Merge the ID to the nonverbal data
+Nonverbal_cloze <- cbind(nverbal,ID[,2])
 
 # Add Cloze probabilities based on the item IDs
 trials <- c(1:2160)
@@ -88,21 +119,34 @@ for (i in trials) {
   Nonverbal_cloze[i,9] <- Cloze_ID[x[,1],2]
 }
 
-#### ####
-dat = bind_rows(verbal, nverbal)
-table(dat$cond)
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
-#---- means ---- 
-dat %>%
-  filter(accuracy == 1 | accuracy == 2) %>% 
+# Merge the Verbal and Nonverbal Data
+
+Verb_Nonverb_Dat = bind_rows(verbal, nverbal)
+table(Verb_Nonverb_Dat$cond)
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+# Summarise of the data
+
+Verb_Nonverb_Dat %>%
+  filter(accuracy == 1 | accuracy == 2) %>% # choose trials with correct answer or with a sysnonym of a correct answer
   group_by(sbj, cond, task) %>%
   summarise(rt = mean(rt)) %>% 
   group_by(cond, task) %>%
   summarise(sd = sd(rt), rt = mean(rt))
 
-#---- quantiles ----
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+# Generate quantile plot
+
+# Separate data into 5 bins
 nbins = 5
-dat_binned <- dat %>%
+dat_binned <- Verb_Nonverb_Dat %>%
   filter(accuracy == 1 | accuracy == 2) %>% 
   group_by(sbj, cond, task) %>% 
   mutate(bin = cut_number(rt,nbins, labels = as.character(1:nbins/5)))
@@ -128,10 +172,13 @@ bin_mean %>%
   scale_color_brewer(type = "qual",palette="Set2") 
 
 
-#---- t test ---- 
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+# Compute t tests for verbal and nonverbal experiment
 
 dat_mean <-   
-  dat %>%
+  Verb_Nonverb_Dat %>%
   filter(accuracy == 1 | accuracy == 2) %>% 
   group_by(sbj, cond, task) %>%
   summarise(mean = mean(rt))
@@ -140,45 +187,3 @@ dat_mean
 with(dat_mean, t.test(mean[cond=="constrained" & task=="verbal"], mean[cond=="unconstrained" & task=="verbal"], paired=T))
 
 with(dat_mean, t.test(mean[cond=="constrained" & task=="nonverbal"], mean[cond=="unconstrained" & task=="nonverbal"], paired=T))
-
-with(dat_mean, t.test(mean[cond=="constrained" & task=="verbal"], mean[cond=="constrained" & task=="nonverbal"], paired=T))
-
-with(dat_mean, t.test(mean[cond=="unconstrained" & task=="verbal"], mean[cond=="unconstrained" & task=="nonverbal"], paired=T))
-
-#--------- Mixed Effects Model ----------------------------
-
-# Verbal task
-Verbal_cloze_correct <- filter(Verbal_cloze, accuracy == 1 | accuracy == 2)
-Verbal_cloze_correct_cons <- filter(Verbal_cloze_correct, condition == 11)
-
-# Option 1
-mixed.lmer <- lmer(rt ~ Cloze + (1|sbj), data = Verbal_cloze_correct_cons)
-summary(mixed.lmer)
-
-(mm_plot <- ggplot(Verbal_cloze_correct_cons, aes(x = Cloze, y = rt)) +
-    aes(color=condition)+
-    facet_wrap(~sbj, nrow=2) +   # a panel for each mountain range
-    geom_point(alpha = 0.5) +
-    theme_classic() +
-    geom_line(data = cbind(Verbal_cloze_correct_cons, pred = predict(mixed.lmer)), aes(y = pred), size = 1) +  # adding predicted line from mixed model 
-    theme(legend.position = "none",
-    panel.spacing = unit(2, "lines"))  # adding space between panels
-)
-
-# Nonverbal task
-
-Nonverbal_cloze_correct <- filter(Nonverbal_cloze, accuracy == 1 | accuracy == 2)
-Nonverbal_cloze_correct_cons <- filter(Nonverbal_cloze_correct, condition == 3)
-
-mixed.lmer2 <- lmer(rt ~ Cloze + (1|sbj), data = Nonverbal_cloze_correct_cons)
-summary(mixed.lmer2)
-
-(mm_plot2 <- ggplot(Nonverbal_cloze_correct_cons, aes(x = Cloze, y = rt)) +
-    aes(color=condition)+
-    facet_wrap(~sbj, nrow=2) +   # a panel for each mountain range
-    geom_point(alpha = 0.5) +
-    theme_classic() +
-    geom_line(data = cbind(Nonverbal_cloze_correct_cons, pred = predict(mixed.lmer2)), aes(y = pred), size = 1) +  # adding predicted line from mixed model 
-    theme(legend.position = "none",
-          panel.spacing = unit(2, "lines"))  # adding space between panels
-)
